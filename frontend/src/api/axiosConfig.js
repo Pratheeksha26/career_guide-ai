49248847746a323acec89dccd2d959a71f32ae76
Pratheeksha <pratheeksha632@gmail.com>
2026-08-29 21:02:40 +0530

    Fix API URLs for Vercel deployment

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : 'http://localhost:8080/api'
+  baseURL: process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : '/api'
 });
 
 instance.interceptors.request.use(
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL 
   ? `${process.env.REACT_APP_API_URL}/api` 
-  : 'http://localhost:8080/api';
+  : '/api';
 
 // Create axios instance
 const api = axios.create({
